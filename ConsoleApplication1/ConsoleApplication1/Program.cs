6d96165217cfbb8d2edc0afe8f8f5b8acbdaa959
bankabt <barnorcsa@gmail.com>
2017-04-23 10:10:47 +0200

    restart

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -6,44 +6,6 @@ using System.Threading.Tasks;
 
 namespace Sample1
 {
-    public class Calc : ICalc
-    {
-        public void Add()
-        {
-            Console.WriteLine("add");
-        }
-
-        public void Div()
-        {
-        }
-
-        public void DivB()
-        {
-        }
-
-        public void DivD()
-        {
-        }
-
-        public void Mult()
-        {
-            throw new NotImplementedException();
-        }
-
-        //hehhe
-    }
-
-    interface ICalc
-    {
-        void Add();
-        void Div();
-        void DivB();
-        void DivD();
-
-        void Mult();
-    }
-
-
     class Program
     {
         static void Main(string[] args)
